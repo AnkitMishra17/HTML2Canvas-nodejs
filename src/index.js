14f0e73eb4d1f6a795afbaa1c8f05254b369b23a
Ankit Mishra <ankit618731@gmail.com>
2019-08-06 02:47:42 +0530

    first-commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const viewsPath = path.join(__dirname, "/views")
+
+app.set('view engine', 'ejs')
+app.set("views", viewsPath)
+
+app.use('/public',express.static(path.join(__dirname, "../public")));
+
+app.get('/', (req,res) =>{
+    res.render('form');
+});
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server started at port ${port}..`)
+  })
+  
