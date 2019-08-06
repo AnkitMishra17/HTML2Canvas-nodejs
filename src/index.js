1d555e7d816733a577732dc17edc9ae8283bb91b
Ankit Mishra <ankit618731@gmail.com>
2019-08-07 00:31:48 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
 
 const viewsPath = path.join(__dirname, "/views")
 
@@ -18,7 +18,6 @@ app.use('/public',express.static(path.join(__dirname, "../public")));
 app.get('/', (req,res) =>{
     res.render('form');
 });
-
 
 app.listen(port, (req, res) => {
     console.log(`Server started at port ${port}..`)
